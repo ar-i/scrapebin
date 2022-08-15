7671c4eb2c0b931b884bf49fd8d22f2a3d060feb
ari <ari@housingsklave.at>
2022-08-15 22:14:28 +0200

    remove all_pastes_for_user for now, add regex_comparison

--- paste.py
+++ paste.py
@@ -4,30 +4,34 @@ import requests
 
 class Paste:
 
-	raw_url = "https://scrape.pastebin.com/api_scrape_item.php?i="
-	metadata_url = "https://scrape.pastebin.com/api_scrape_item_meta.php?i="
+    raw_url = "https://scrape.pastebin.com/api_scrape_item.php?i="
+    metadata_url = "https://scrape.pastebin.com/api_scrape_item_meta.php?i="
 
-	def __init__(self, date, key, size, expire, title, user):
-		self.date = date
-		self.key = key
-		self.size = size
-		self.expire = expire or "none"
-		self.title = title or "none"
-		self.user = user or "none"
+    def __init__(self, date, key, size, expire, title, user):
+        self.date = date
+        self.key = key
+        self.size = size
+        self.expire = expire or "none"
+        self.title = title or "none"
+        self.user = user or "none"
 
-	def fetch_content(self):
-		'''Fetches the (raw) content for the paste belonging to the provided
-		key. Returns the data as a string'''
-		print(f"Currently fetching: {self.key}")
-		full_content = requests.get(self.raw_url + self.key).text
-		return full_content
+    def fetch_content(self):
+        '''Fetches the (raw) content for the paste belonging to the provided
+        key. Returns the data as a string'''
+        full_content = requests.get(self.raw_url + self.key).text
+        return full_content
 
-	def store_locally(key, content):
-		'''Stores the content for the paste belonging to the provided key in a
-		local file. Returns the full file path as a string'''
-		pass
+    def store_locally(self, path, content):
+        '''Stores the content for the paste belonging to the provided key in a
+        local file. Returns the full file path as a string'''
+        try:
+            with open(path + self.key + ".txt", "w") as local_file:
+                local_file.write(content)
+        except Exception as error:
+            print(f"[!] An error occured: {error}!")
 
-	def all_pastes_for_user(username, database_object):
-		'''Queries the database for all pastes authored by a supplied user,
-		returns a list of URLs'''
-		pass
+    def regex_comparison(self,regex):
+        '''Compares the content of the paste against a list of regular
+        expressions for matches. Unclear if the comparison will be done before
+        the local storage / database insertion or by manually checking all
+        files.'''

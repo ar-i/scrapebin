f238faa748b5812f3331cbaf10019b766d27d280
ari <ari@housingsklave.at>
2022-08-15 19:14:04 +0200

    Fixed tabs/spaces, added fetch_content

--- paste.py
+++ paste.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python3
 
+import requests
+
 class Paste:
 
-    raw_url = "https://scrape.pastebin.com/api_scrape_item.php?i="
-    metadata_url = "https://scrape.pastebin.com/api_scrape_item_meta.php?i="
+	raw_url = "https://scrape.pastebin.com/api_scrape_item.php?i="
+	metadata_url = "https://scrape.pastebin.com/api_scrape_item_meta.php?i="
 
-    def __init__(self, date, key, size, expire, title, user):
-        self.date = date
-        self.key = key
-        self.size = size
-        self.expire = expire or "none"
-        self.title = title or "none"
-        self.user = user or "none"
+	def __init__(self, date, key, size, expire, title, user):
+		self.date = date
+		self.key = key
+		self.size = size
+		self.expire = expire or "none"
+		self.title = title or "none"
+		self.user = user or "none"
 
-    def fetch_content(self):
-        '''Fetches the (raw) content for the paste belonging to the provided
-        key. Returns the data as a string'''
-        pass
+	def fetch_content(self):
+		'''Fetches the (raw) content for the paste belonging to the provided
+		key. Returns the data as a string'''
+		print(f"Currently fetching: {self.key}")
+		full_content = requests.get(self.raw_url + self.key).text
+		return full_content
 
-    def store_locally(key, content):
-        '''Stores the content for the paste belonging to the provided key in a
-        local file. Returns the full file path as a string'''
-        pass
+	def store_locally(key, content):
+		'''Stores the content for the paste belonging to the provided key in a
+		local file. Returns the full file path as a string'''
+		pass
 
-    def all_pastes_for_user(username, database_object):
-        '''Queries the database for all pastes authored by a supplied user,
-        returns a list of URLs'''
-        pass
+	def all_pastes_for_user(username, database_object):
+		'''Queries the database for all pastes authored by a supplied user,
+		returns a list of URLs'''
+		pass
